d8e24ba06d11dcce29269fcd024f4f56391a8b97
Nick <nicholasjohnrichardson@gmail.com>
2018-09-09 20:28:52 +0100

    styled image component

--- src/phoneimage.js
+++ src/phoneimage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './phoneimage.css';
 
 const PhoneImage = props => {
   return (
--- src/phoneinfo.js
+++ src/phoneinfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PhoneInfo = props => {
   return (
     <div className="PhoneInfo">
-      <p>{props.groupname}</p>
+      <h1>{props.groupname}</h1>
       <p>{props.description}</p>
     </div>
   )
