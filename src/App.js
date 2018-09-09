11b1551772d1feab58b11f6fefd34e08792671af
Nick <nicholasjohnrichardson@gmail.com>
2018-09-09 17:34:57 +0100

    page renders colourchange buttons

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Gold from './images/Apple_iPhone_8_Gold-full-product-front.png';
 import Silver from './images/Apple_iPhone_8_Silver_WS2-full-product-front.png';
 import Grey from './images/Apple_iPhone_8_Space_Grey_WS2-full-product-front.png';
 
-console.log(PhoneData);
-
 class App extends Component {
   render() {
     return (
@@ -26,13 +24,22 @@ class Page extends Component {
       colour: Gold
     }
   }
+
   render() {
+    function handleClick(e) {
+      e.preventDefault();
+      console.log(e.target.value);
+    }
+
     return (
       <div className="Page">
         <img src={this.state.colour} alt=""></img>
         <p>{this.state.groupname}</p>
         <p>{this.state.phonetype.displayDescription}</p>
         <p>Colour: {this.state.phonetype.colourName}</p>
+        <button value='gold' onClick={handleClick}>Gold</button>
+        <button value='silver' onClick={handleClick}>Silver</button>
+        <button value='grey' onClick={handleClick}>Grey</button>
         <p>Capacity: {this.state.phonetype.memory}</p>
         <p>from £{this.state.phonetype.priceInfo.hardwarePrice.oneOffPrice.gross} upfront cost</p>
         <p>When you pay £{this.state.phonetype.priceInfo.bundlePrice.monthlyPrice.gross} a month</p>
