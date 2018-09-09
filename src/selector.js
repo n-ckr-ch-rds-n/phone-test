f63f4a2df403ddeb54191a5f14d8752c4bb924b4
Nick <nicholasjohnrichardson@gmail.com>
2018-09-09 20:49:36 +0100

    styled selector

--- src/selector.js
+++ src/selector.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import './selector.css';
 
 const Selector = props => {
   return (
     <div className="Selector">
-      <p>Colour: {props.colour}</p>
-        <button value='gold' onClick={props.changeColour}>Gold</button>
-        <button value='silver' onClick={props.changeColour}>Silver</button>
-        <button value='grey' onClick={props.changeColour}>Grey</button>
-      <p>Capacity: {props.capacity}</p>
-        <button value='64' onClick={props.changeCapacity}>64</button>
-        <button value='256' onClick={props.changeCapacity}>256</button>
+      <div className="Colour">
+        <p>Colour: <span className='bold'>{props.colour}</span></p>
+          <button className="gold" value='gold' onClick={props.changeColour}></button>
+          <button className="silver" value='silver' onClick={props.changeColour}></button>
+          <button className="grey" value='grey' onClick={props.changeColour}></button>
+      </div>
+      <div className="Capacity">
+        <p>Capacity: <span className='bold'>{props.capacity}</span></p>
+          <button className="capacitybutton" value='64' onClick={props.changeCapacity}>64</button>
+          <button className="capacitybutton" value='256' onClick={props.changeCapacity}>256</button>
+      </div>
     </div>
   )
 }
