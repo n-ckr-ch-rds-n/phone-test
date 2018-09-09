91aea1a55266394ab0901b068db7ce00b954509b
Nick <nicholasjohnrichardson@gmail.com>
2018-09-09 20:04:16 +0100

    extracted page class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import PhoneData from './phonedata';
-import PriceBlock from './priceblock';
-import Selector from './selector';
-import PhoneImage from './phoneimage';
-import PhoneInfo from './phoneinfo';
-import Gold from './images/Apple_iPhone_8_Gold-full-product-front.png';
-import Silver from './images/Apple_iPhone_8_Silver_WS2-full-product-front.png';
-import Grey from './images/Apple_iPhone_8_Space_Grey_WS2-full-product-front.png';
+import Page from './page.js';
 
 class App extends Component {
   render() {
@@ -16,75 +9,6 @@ class App extends Component {
         <Page/>
       </div>
     );
-  }
-}
-
-class Page extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      groupname: PhoneData[0].groupName,
-      phonetype: PhoneData[0].deviceSummary[0],
-      colour: Gold
-    }
-    this.changeColour = this.changeColour.bind(this)
-    this.changeCapacity = this.changeCapacity.bind(this)
-  }
-
-  changeColour(e) {
-    e.preventDefault();
-    if (e.target.value === 'gold' && this.state.phonetype.memory === '64GB') {
-      this.setState({ colour: Gold, phonetype: PhoneData[0].deviceSummary[0] });
-    } else if (e.target.value === 'silver' && this.state.phonetype.memory === '64GB') {
-      this.setState({ colour: Silver, phonetype: PhoneData[0].deviceSummary[1] });
-    } else if (e.target.value === 'grey' && this.state.phonetype.memory === '64GB') {
-      this.setState({ colour: Grey, phonetype: PhoneData[0].deviceSummary[2] });
-    } else if (e.target.value === 'gold' && this.state.phonetype.memory === '256GB') {
-      this.setState({ colour: Gold, phonetype: PhoneData[0].deviceSummary[3] });
-    } else if (e.target.value === 'silver' && this.state.phonetype.memory === '256GB') {
-      this.setState({ colour: Silver, phonetype: PhoneData[0].deviceSummary[4] });
-    } else if (e.target.value === 'grey' && this.state.phonetype.memory === '256GB') {
-      this.setState({ colour: Grey, phonetype: PhoneData[0].deviceSummary[5] });
-    }
-  }
-
-  changeCapacity(e) {
-    e.preventDefault();
-    if (e.target.value === '64' && this.state.colour === Gold) {
-      this.setState({ phonetype: PhoneData[0].deviceSummary[0] });
-    } else if (e.target.value === '64' && this.state.colour === Silver) {
-      this.setState({ phonetype: PhoneData[0].deviceSummary[1] });
-    } else if (e.target.value === '64' && this.state.colour === Grey) {
-      this.setState({ phonetype: PhoneData[0].deviceSummary[2] });
-    } else if (e.target.value === '256' && this.state.colour === Gold) {
-      this.setState({ phonetype: PhoneData[0].deviceSummary[3] });
-    } else if (e.target.value === '256' && this.state.colour === Silver) {
-      this.setState({ phonetype: PhoneData[0].deviceSummary[4] });
-    } else if (e.target.value === '256' && this.state.colour === Grey) {
-      this.setState({ phonetype: PhoneData[0].deviceSummary[5] });
-    }
-  }
-
-  render() {
-    return (
-      <div className="Page">
-        <PhoneImage colour={this.state.colour}/>
-        <PhoneInfo
-          groupname={this.state.groupname}
-          description={this.state.phonetype.displayDescription}
-        />
-        <Selector
-          colour={this.state.phonetype.colourName}
-          capacity={this.state.phonetype.memory}
-          changeColour={this.changeColour}
-          changeCapacity={this.changeCapacity}
-        />
-        <PriceBlock
-          upfront={this.state.phonetype.priceInfo.hardwarePrice.oneOffPrice.gross}
-          monthly={this.state.phonetype.priceInfo.bundlePrice.monthlyPrice.gross}
-        />
-      </div>
-    )
   }
 }
 
