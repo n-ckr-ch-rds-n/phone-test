9bf7d9da55a43d734664ebdbc931614977cb13e4
Nick <nicholasjohnrichardson@gmail.com>
2018-09-09 16:09:20 +0100

    page displays phone name

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PhoneData from './phonedata'
+
+console.log(PhoneData);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Page/>
       </div>
     );
+  }
+}
+
+class Page extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phonedata: PhoneData
+    }
+  }
+  render() {
+    return (
+      <div className="Page">
+        <p>{this.state.phonedata[0].groupName}</p>
+      </div>
+    )
   }
 }
 
