2c49be49b6af7b81ed9859d9fe310b27a1368acf
Nick <nicholasjohnrichardson@gmail.com>
2018-09-10 12:20:09 +0100

    more unit tests for page component

--- src/Page.test.js
+++ src/Page.test.js
@@ -4,6 +4,7 @@ import Page from './page';
 import PhoneImage from './phoneimage';
 import PhoneInfo from './phoneinfo';
 import Selector from './selector';
+import PriceBlock from './priceblock';
 
 describe('<Page />', () => {
 
@@ -26,7 +27,7 @@ describe('<Page />', () => {
   })
 
   it('passes image file as props to PhoneImage', () => {
-    expect(wrapper.find(PhoneImage).props().colour).toEqual("Apple_iPhone_8_Gold-full-product-front.png");
+    expect(wrapper.find(PhoneImage).props().colour).toEqual(wrapper.state().colour);
   })
 
   it('renders a PhoneInfo component', () => {
@@ -35,11 +36,34 @@ describe('<Page />', () => {
 
   it('passes props to PhoneInfo', () => {
     const infoprops = wrapper.find(PhoneInfo).props();
-    expect(infoprops.groupname).toEqual('Apple iPhone 8');
+    expect(infoprops.groupname).toEqual(wrapper.state().groupname);
   })
 
   it('renders a Selector component', () => {
     expect(wrapper.find(Selector).length).toEqual(1);
   })
 
+  it('passes colour props to Selector', () => {
+    const selectorprops = wrapper.find(Selector).props();
+    expect(selectorprops.colour).toEqual(wrapper.state().phonetype.colourName);
+  })
+
+  it('passes capacity props to Selector', () => {
+    const selectorprops = wrapper.find(Selector).props();
+    expect(selectorprops.capacity).toEqual(wrapper.state().phonetype.memory);
+  })
+
+  it('renders a PriceBlock component', () => {
+    expect(wrapper.find(PriceBlock).length).toEqual(1);
+  })
+
+  it('passes upfront price props to PriceBlock', () => {
+    const priceblockprops = wrapper.find(PriceBlock).props();
+    expect(priceblockprops.upfront).toEqual(wrapper.state().phonetype.priceInfo.hardwarePrice.oneOffPrice.gross);
+  })
+
+  it('passes upfront price props to PriceBlock', () => {
+    const priceblockprops = wrapper.find(PriceBlock).props();
+    expect(priceblockprops.monthly).toEqual(wrapper.state().phonetype.priceInfo.bundlePrice.monthlyPrice.gross);
+  })
 })
